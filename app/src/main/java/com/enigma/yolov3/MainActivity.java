420609893e67bc9adce55ff53cecdc5fcb2e3724
badrul <mbadrulamin123@gmail.com>
2022-10-15 23:23:00 +0800

    YOLOv3

--- app/src/main/java/com/enigma/yolov3/MainActivity.java
+++ app/src/main/java/com/enigma/yolov3/MainActivity.java
@@ -3,6 +3,7 @@ package com.enigma.yolov3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.SurfaceView;
 import android.view.View;
@@ -14,43 +15,55 @@ import org.opencv.android.JavaCameraView;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfFloat;
+import org.opencv.core.MatOfInt;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.MatOfRect2d;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Rect2d;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.dnn.Dnn;
+import org.opencv.dnn.Net;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.utils.Converters;
 
-public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2{
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
     CameraBridgeViewBase cameraBridgeViewBase;
     BaseLoaderCallback baseLoaderCallback;
-    int counter = 0;
+    boolean startYolo = false;
+    boolean firstTimeYolo = false;
+    Net tinyYolo;
 
-    boolean startCanny = false;
 
-    private static String TAG = "MainActivity";
-    static{
-        if (OpenCVLoader.initDebug()){
-            Log.d(TAG, "opencv installed successfully");
-        }else{
-            Log.d(TAG, "opencv is not installed");
-        }
-    }
+    public void YOLO(View Button) {
 
-    public void Canny(View Button) {
+        if (startYolo == false) {
+            startYolo = true;
+            if (firstTimeYolo == false) {
+                firstTimeYolo = true;
+                String tinyYoloCfg = Environment.getExternalStorageDirectory() + "/dnns/yolov3-tiny.cfg";
+                String tinyYoloWeights = Environment.getExternalStorageDirectory() + "/dnns/yolov3-tiny.weights";
 
-        if (startCanny == false) {
-            startCanny = true;
+                tinyYolo = Dnn.readNetFromDarknet(tinyYoloCfg, tinyYoloWeights);
+            }
         } else {
-
-            startCanny = false;
-
-
+            startYolo = false;
         }
     }
 
-        @Override
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        cameraBridgeViewBase = (JavaCameraView)findViewById(R.id.CameraView);
+        cameraBridgeViewBase = (JavaCameraView) findViewById(R.id.CameraView);
         cameraBridgeViewBase.setVisibility(SurfaceView.VISIBLE);
         cameraBridgeViewBase.setCvCameraViewListener(this);
 
@@ -61,7 +74,7 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
             public void onManagerConnected(int status) {
                 super.onManagerConnected(status);
 
-                switch(status){
+                switch (status) {
 
                     case BaseLoaderCallback.SUCCESS:
                         cameraBridgeViewBase.enableView();
@@ -81,31 +94,108 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
 
-//        Mat frame = inputFrame.rgba();
-//
-//        if (counter % 2 == 0){
-//
-//            Core.flip(frame, frame, 1);
-//            Imgproc.cvtColor(frame, frame, Imgproc.COLOR_RGBA2GRAY);
-//
-//
-//        }
-//
-//        counter = counter + 1;
-
         Mat frame = inputFrame.rgba();
 
-        if (startCanny == true) {
+        if (startYolo == true) {
 
-            Imgproc.cvtColor(frame, frame, Imgproc.COLOR_RGBA2GRAY);
-            Imgproc.Canny(frame, frame, 100, 80);
+            Imgproc.cvtColor(frame, frame, Imgproc.COLOR_RGBA2RGB);
 
+            Mat imageBlob = Dnn.blobFromImage(frame, 0.00392, new Size(416, 416), new Scalar(0, 0, 0),/*swapRB*/false, /*crop*/false);
+
+            tinyYolo.setInput(imageBlob);
+
+            java.util.List<Mat> result = new java.util.ArrayList<Mat>(2);
+
+            List<String> outBlobNames = new java.util.ArrayList<>();
+            outBlobNames.add(0, "yolo_16");
+            outBlobNames.add(1, "yolo_23");
+
+            tinyYolo.forward(result, outBlobNames);
+
+            float confThreshold = 0.3f;
+
+            List<Integer> clsIds = new ArrayList<>();
+            List<Float> confs = new ArrayList<>();
+            List<Rect2d> rects2d = new ArrayList<>();
+
+            for (int i = 0; i < result.size(); ++i) {
+
+                Mat level = result.get(i);
+
+                for (int j = 0; j < level.rows(); ++j) {
+                    Mat row = level.row(j);
+                    Mat scores = row.colRange(5, level.cols());
+
+                    Core.MinMaxLocResult mm = Core.minMaxLoc(scores);
+
+
+                    float confidence = (float) mm.maxVal;
+
+
+                    Point classIdPoint = mm.maxLoc;
+
+
+                    if (confidence > confThreshold) {
+                        int centerX = (int) (row.get(0, 0)[0] * frame.cols());
+                        int centerY = (int) (row.get(0, 1)[0] * frame.rows());
+                        int width = (int) (row.get(0, 2)[0] * frame.cols());
+                        int height = (int) (row.get(0, 3)[0] * frame.rows());
+
+
+                        int left = centerX - width / 2;
+                        int top = centerY - height / 2;
+
+                        clsIds.add((int) classIdPoint.x);
+                        confs.add((float) confidence);
+
+
+                        rects2d.add(new Rect2d(left, top, width, height));
+                    }
+                }
+            }
+            int ArrayLength = confs.size();
+
+            if (ArrayLength >= 1) {
+                // Apply non-maximum suppression procedure.
+                float nmsThresh = 0.2f;
+
+
+                MatOfFloat confidences = new MatOfFloat(Converters.vector_float_to_Mat(confs));
+
+
+                Rect2d[] boxesArray = rects2d.toArray(new Rect2d[0]);
+
+                MatOfRect2d boxes = new MatOfRect2d(boxesArray);
+
+                MatOfInt indices = new MatOfInt();
+
+
+                Dnn.NMSBoxes(boxes, confidences, confThreshold, nmsThresh, indices);
+
+
+                // Draw result boxes:
+                int[] ind = indices.toArray();
+                for (int i = 0; i < ind.length; ++i) {
+
+                    int idx = ind[i];
+                    Rect2d box = boxesArray[idx];
+
+                    int idGuy = clsIds.get(idx);
+
+                    float conf = confs.get(idx);
+
+
+                    List<String> cocoNames = Arrays.asList("a person", "a bicycle", "a motorbike", "an airplane", "a bus", "a train", "a truck", "a boat", "a traffic light", "a fire hydrant", "a stop sign", "a parking meter", "a car", "a bench", "a bird", "a cat", "a dog", "a horse", "a sheep", "a cow", "an elephant", "a bear", "a zebra", "a giraffe", "a backpack", "an umbrella", "a handbag", "a tie", "a suitcase", "a frisbee", "skis", "a snowboard", "a sports ball", "a kite", "a baseball bat", "a baseball glove", "a skateboard", "a surfboard", "a tennis racket", "a bottle", "a wine glass", "a cup", "a fork", "a knife", "a spoon", "a bowl", "a banana", "an apple", "a sandwich", "an orange", "broccoli", "a carrot", "a hot dog", "a pizza", "a doughnut", "a cake", "a chair", "a sofa", "a potted plant", "a bed", "a dining table", "a toilet", "a TV monitor", "a laptop", "a computer mouse", "a remote control", "a keyboard", "a cell phone", "a microwave", "an oven", "a toaster", "a sink", "a refrigerator", "a book", "a clock", "a vase", "a pair of scissors", "a teddy bear", "a hair drier", "a toothbrush");
+
+                    int intConf = (int) (conf * 100);
+
+                    Imgproc.putText(frame, cocoNames.get(idGuy) + " " + intConf + "%", box.tl(), Core.FONT_HERSHEY_SIMPLEX, 2, new Scalar(255, 255, 0), 2);
+
+                    Imgproc.rectangle(frame, box.tl(), box.br(), new Scalar(255, 0, 0), 2);
+
+                }
+            }
         }
-
-
-
-
-
 
         return frame;
     }
@@ -113,7 +203,15 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
 
     @Override
     public void onCameraViewStarted(int width, int height) {
+        if (startYolo == true) {
 
+            String tinyYoloCfg = Environment.getExternalStorageDirectory() + "/dnns/yolov3-tiny.cfg";
+            String tinyYoloWeights = Environment.getExternalStorageDirectory() + "/dnns/yolov3-tiny.weights";
+
+            tinyYolo = Dnn.readNetFromDarknet(tinyYoloCfg, tinyYoloWeights);
+
+
+        }
     }
 
 
@@ -127,15 +225,11 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     protected void onResume() {
         super.onResume();
 
-        if (!OpenCVLoader.initDebug()){
-            Toast.makeText(getApplicationContext(),"There's a problem, yo!", Toast.LENGTH_SHORT).show();
-        }
-
-        else
-        {
+        if (!OpenCVLoader.initDebug()) {
+            Toast.makeText(getApplicationContext(), "There's a problem, yo!", Toast.LENGTH_SHORT).show();
+        } else {
             baseLoaderCallback.onManagerConnected(baseLoaderCallback.SUCCESS);
         }
-
 
 
     }
@@ -143,7 +237,7 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     @Override
     protected void onPause() {
         super.onPause();
-        if(cameraBridgeViewBase!=null){
+        if (cameraBridgeViewBase != null) {
 
             cameraBridgeViewBase.disableView();
         }
@@ -154,7 +248,7 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if (cameraBridgeViewBase!=null){
+        if (cameraBridgeViewBase != null) {
             cameraBridgeViewBase.disableView();
         }
     }
